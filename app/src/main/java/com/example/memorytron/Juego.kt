1da9d5533385c563ef6a43d97f7b2b7f83a8c24b
rauul <rauulssanchezz@gmaiil.com>
2023-11-24 12:01:22 +0100

    yvubh

--- app/src/main/java/com/example/memorytron/Juego.kt
+++ app/src/main/java/com/example/memorytron/Juego.kt
@@ -1,6 +1,8 @@
 package com.example.memorytron
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -12,103 +14,167 @@ import kotlin.random.Random
 
 class Juego : AppCompatActivity() {
 
-    lateinit var imgs :MutableList<Int>
+    lateinit var imgs: MutableList<Int>
+    lateinit var ultimg: ImageView
+    var primerclick = false
+    lateinit var imageViews: MutableList<ImageView>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_juego)
-        imgs= mutableListOf(R.drawable.pinguino, R.drawable.mono, R.drawable.medusa,
-            R.drawable.gato, R.drawable.doberman, R.drawable.cerdo, R.drawable.pinguino, R.drawable.mono, R.drawable.medusa,
-            R.drawable.gato, R.drawable.doberman, R.drawable.cerdo)
+        imgs = mutableListOf(
+            R.drawable.pinguino,
+            R.drawable.mono,
+            R.drawable.medusa,
+            R.drawable.gato,
+            R.drawable.doberman,
+            R.drawable.cerdo,
+            R.drawable.pinguino,
+            R.drawable.mono,
+            R.drawable.medusa,
+            R.drawable.gato,
+            R.drawable.doberman,
+            R.drawable.cerdo
+        )
         imgs.shuffle()
+        imageViews = mutableListOf(
+            findViewById<ImageView>(R.id.ct1img1),
+            findViewById<ImageView>(R.id.ct1img2),
+            findViewById<ImageView>(R.id.ct1img3),
+            findViewById<ImageView>(R.id.ct1img4),
+            findViewById<ImageView>(R.id.ct2img1),
+            findViewById<ImageView>(R.id.ct2img2),
+            findViewById<ImageView>(R.id.ct2img3),
+            findViewById<ImageView>(R.id.ct2img4),
+            findViewById<ImageView>(R.id.ct3img1),
+            findViewById<ImageView>(R.id.ct3img2),
+            findViewById<ImageView>(R.id.ct3img3),
+            findViewById<ImageView>(R.id.ct3img4)
+        )
+
 
     }
 
+    fun comprobar(img1: ImageView, img2: ImageView): Boolean {
+        var drawable1 = img1.drawable
+        var drawable2 = img2.drawable
+        var bitmap1 = Bitmap.createBitmap(
+            drawable1.intrinsicWidth,
+            drawable1.intrinsicHeight,
+            Bitmap.Config.ARGB_8888
+        )
+        var bitmap2 = Bitmap.createBitmap(
+            drawable2.intrinsicWidth,
+            drawable2.intrinsicHeight,
+            Bitmap.Config.ARGB_8888
+        )
+        var canvas1 = android.graphics.Canvas(bitmap1)
+        var canvas2 = android.graphics.Canvas(bitmap2)
+        drawable1.setBounds(0, 0, canvas1.width, canvas1.height)
+        drawable2.setBounds(0, 0, canvas2.width, canvas2.height)
+        drawable1.draw(canvas1)
+        drawable2.draw(canvas2)
+        return bitmap1.sameAs(bitmap2)
+    }
+    fun primeraparte(pos: Int) {
+        imageViews[pos].setImageResource(imgs[pos])
+    }
 
+    fun segundaparte(pos: Int) {
+        if (primerclick) {
+            var iguales=comprobar(imageViews[pos], ultimg)
+            if (!iguales){
+                imageViews[pos].setImageResource(R.drawable.parteatras)
+                ultimg.setImageResource(R.drawable.parteatras)
+            }
+        } else {
+            primerclick = true
+        }
+        ultimg = imageViews[pos]
+    }
 
     fun pulsado(view: View) {
 
 
+        when (view.id) {
+            R.id.ct1img1 -> {
 
-        when(view.id) {
-                R.id.ct1img1 -> {
-                    var imagen = findViewById<ImageView>(R.id.ct1img1)
+                primeraparte(0)
 
-                    imagen.setImageResource(imgs[0])
-                    Thread.sleep(1000)
+                view.postDelayed({ segundaparte(0) }, 500)
+            }
 
-                }
+            R.id.ct1img2 -> {
 
-                R.id.ct1img2 -> {
+                primeraparte(1)
 
-                    var imagen = findViewById<ImageView>(R.id.ct1img2)
+                view.postDelayed({ segundaparte(1) }, 500)
+            }
 
-                    imagen.setImageResource(imgs[1])
-
-                }
             R.id.ct1img3 -> {
-                var imagen = findViewById<ImageView>(R.id.ct1img3)
+                primeraparte(2)
 
-                    imagen.setImageResource(imgs[2])
-
+                view.postDelayed({ segundaparte(2) }, 500)
             }
 
             R.id.ct1img4 -> {
-                var imagen = findViewById<ImageView>(R.id.ct1img4)
+                primeraparte(3)
 
-                imagen.setImageResource(imgs[3])
-
+                view.postDelayed({ segundaparte(3) }, 500)
             }
+
             R.id.ct2img1 -> {
-                var imagen = findViewById<ImageView>(R.id.ct2img1)
-                imagen.setImageResource(imgs[4])
+                primeraparte(4)
 
+                view.postDelayed({ segundaparte(4) }, 500)
             }
+
             R.id.ct2img2 -> {
-                var imagen = findViewById<ImageView>(R.id.ct2img2)
+                primeraparte(5)
 
-                imagen.setImageResource(imgs[5])
-
+                view.postDelayed({ segundaparte(5) }, 500)
             }
+
             R.id.ct2img3 -> {
-                var imagen = findViewById<ImageView>(R.id.ct2img3)
+                primeraparte(6)
 
-                imagen.setImageResource(imgs[6])
-
+                view.postDelayed({ segundaparte(6) }, 500)
             }
+
             R.id.ct2img4 -> {
-                var imagen = findViewById<ImageView>(R.id.ct2img4)
+                primeraparte(7)
 
-                imagen.setImageResource(imgs[7])
-
+                view.postDelayed({ segundaparte(7) }, 500)
             }
+
             R.id.ct3img1 -> {
-                var imagen = findViewById<ImageView>(R.id.ct3img1)
+                primeraparte(8)
 
-                imagen.setImageResource(imgs[8])
-
+                view.postDelayed({ segundaparte(8) }, 500)
             }
+
             R.id.ct3img2 -> {
-                var imagen = findViewById<ImageView>(R.id.ct3img2)
+                primeraparte(9)
 
-                imagen.setImageResource(imgs[9])
-
+                view.postDelayed({ segundaparte(9) }, 500)
             }
+
             R.id.ct3img3 -> {
-                var imagen = findViewById<ImageView>(R.id.ct3img3)
+                primeraparte(10)
 
-                imagen.setImageResource(imgs[10])
-
+                view.postDelayed({ segundaparte(10) }, 500)
             }
+
             R.id.ct3img4 -> {
-                var imagen = findViewById<ImageView>(R.id.ct3img4)
+                primeraparte(11)
 
-                    imagen.setImageResource(imgs[11])
-
-                }
+                view.postDelayed({ segundaparte(11) }, 500)
             }
 
-            }
         }
+
+
+    }
+}
 
 
 
