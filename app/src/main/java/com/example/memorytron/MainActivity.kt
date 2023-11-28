087d10f11fb4a54a65b164ad66298d3d116b4d81
EAG <rauulssanchezz@gmail.com>
2023-11-28 19:53:09 +0100

    jjjj

--- app/src/main/java/com/example/memorytron/MainActivity.kt
+++ app/src/main/java/com/example/memorytron/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.memorytron
 
 import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    var mediaPlayer:MediaPlayer?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mediaPlayer= MediaPlayer.create(this,R.raw.inicio)
+        mediaPlayer?.start()
     }
 
     fun jugar(view: View) {
-
-
         val intent=Intent(this,Juego::class.java)
 
         startActivity(intent)
